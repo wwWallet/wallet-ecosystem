d688339555dba88e25e0d62d99fcfb6fae561c90
Emmanouil Koukoularis <koukoularisman@gmail.com>
2023-09-06 18:14:42 +0300

    added verifier

---
+++ wallet-enterprise-configurations/acme-verifier/src/configuration/locale.ts
@@ -0,0 +1,40 @@
+const locale = {
+	en: {
+		header: {
+			title: "ACME Verifier",
+			subtitle: "Send your credentials",
+		},
+		footer: {
+			services: "Services",
+			documentation: "Documentation",
+			adminLogin: "Verifier Panel",
+			information: "Information",
+			participatingInst: "Participating Institutions",
+			termsOfUse: "Terms of Use",
+			contact: "Contact",
+			web: "Web",
+			emailForOrgs: "E-mail for Institutions"
+		},
+		index: {
+			header: "ACME Verifier",
+			phrase: "I want to present credentials",
+			proceed: "Proceed",
+			heading: "ACME Verifier",
+			paragraph: "",
+			demo_text: ""
+		},
+		login: {
+			title: "Login",
+			description: "",
+			btnText: "Login",
+			error: {
+				emptyUsername: "Username is empty",
+				emptyPassword: "Password is empty",
+				invalidCredentials: "Invalid credentials",
+				networkError: "Network error occured",
+			}
+		}
+	}
+}
+
+export default locale;
---
+++ wallet-enterprise-configurations/acme-verifier/src/configuration/resources/data.ts
@@ -0,0 +1,49 @@
+
+
+type DiplomaEntry = {
+	blueprintID: string;
+	certificateId: string;
+	firstName: string;
+	familyName: string;
+	title: string;
+	taxisId: string;
+	institutionCode: string;
+	institutionName: string;
+	grade: string;
+	level: string;
+}
+
+type VIDEntry = {
+	personalIdentifier: string;
+	firstName: string;
+	familyName: string;
+	taxisId: string;	
+	birthdate: string;
+}
+
+
+
+const vidRepository: VIDEntry[] = [
+	{
+		personalIdentifier: "032429484252432",
+		firstName: "George",
+		familyName: "Dokimastikos",
+		taxisId: "432432432423",
+		birthdate: "18-5-1990"
+	}
+]
+
+
+
+
+async function getVIDByTaxisId(taxisid: string) {
+	return vidRepository.filter(s => s.taxisId == taxisid)[0];
+}
+
+
+
+export { 
+	getVIDByTaxisId,
+	DiplomaEntry,
+	VIDEntry
+}
