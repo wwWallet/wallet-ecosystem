b7ba15f68952b423caf47c196ffe0e413b144c7b
pstamatop <pstamatop@gmail.com>
2023-11-20 11:54:15 +0200

    VID issuer landing text

--- wallet-enterprise-configurations/vid-issuer/src/configuration/locale.ts
+++ wallet-enterprise-configurations/vid-issuer/src/configuration/locale.ts
@@ -21,10 +21,10 @@ const locale = {
 		},
 		index: {
 			header: "National VID Issuer",
-			phrase: "I am a greek citizen",
+			phrase: "I want to receive a Verifiable ID",
 			proceed: "Proceed",
 			heading: "National Verifiable ID Issuer",
-			paragraph: "This is a portal where Greek citizen can receive their digital Verifiable ID in their wallet. To proceed you must first have an EBSI conformant digital wallet. This Verifiable ID will contain a the Social Security Number as a personal identifier.",
+			paragraph: "This is a portal where citizens can receive their digital Verifiable ID in their wallet. To proceed you must first have an EBSI conformant digital wallet. This Verifiable ID will contain a the Social Security Number as a personal identifier.",
 			demo_text: "This is a National VID implementation for demonstration puproses. It has been developed in the framework of EBSI Early Adopters Program and the Multi University Pilot, with the endorsement of the Greek Ministry of Education. The purpose of this application is to issue a Verifiable ID for authentication puproses."
 		},
 		login: {
