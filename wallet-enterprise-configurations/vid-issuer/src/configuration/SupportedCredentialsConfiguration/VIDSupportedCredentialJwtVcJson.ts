da1da4322a0ce61b1bc5c18315dd8fb6490315f2
kkmanos <koukoularisman@gmail.com>
2024-02-23 16:48:25 +0200

    added schema on vid credential

--- wallet-enterprise-configurations/vid-issuer/src/configuration/SupportedCredentialsConfiguration/VIDSupportedCredentialJwtVcJson.ts
+++ wallet-enterprise-configurations/vid-issuer/src/configuration/SupportedCredentialsConfiguration/VIDSupportedCredentialJwtVcJson.ts
@@ -86,6 +86,10 @@ export class VIDSupportedCredentialJwtVcJson implements SupportedCredentialProto
 				...vid,
 				"id": holderDID,
 			},
+			"credentialSchema": {
+				"id": "https://api-pilot.ebsi.eu/trusted-schemas-registry/v2/schemas/z8Y6JJnebU2UuQQNc2R8GYqkEiAMj3Hd861rQhsoNWxsM",
+				"type": "JsonSchema",
+			}
 		};
 		const { jws } = await this.getCredentialIssuerConfig().getCredentialSigner()
 			.sign(payload, {});
