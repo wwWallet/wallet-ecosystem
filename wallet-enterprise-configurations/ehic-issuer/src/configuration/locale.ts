83d29197c6e31d4501812c88d3be603b507ef5f6
gregory1996 <grigoriskatrakazas@gmail.com>
2024-03-22 16:34:38 +0200

    update vid and ehic issuer

--- wallet-enterprise-configurations/ehic-issuer/src/configuration/locale.ts
+++ wallet-enterprise-configurations/ehic-issuer/src/configuration/locale.ts
@@ -20,12 +20,12 @@ const locale = {
 			emailForOrgs: "E-mail for Institutions"
 		},
 		index: {
-			header: "EHIC Issuer",
-			phrase: "I want to receive an EHIC Card",
+			header: "eEHIC Issuer",
+			phrase: "I want to receive my eEHIC",
 			proceed: "Proceed",
-			heading: "EHIC Card Issuer",
-			paragraph: "This is a portal where citizens can receive their digital EHIC Card in their wallet. To proceed you must first have an EBSI conformant digital wallet. This Verifiable EHIC Card will contain a the Social Security Number as a personal identifier.",
-			demo_text: "This is an Issuer implementation for demonstration purposes. It has been developed in the framework of EBSI Early Adopters Program and the Multi University Pilot, with the endorsement of the Greek Ministry of Education. The purpose of this application is to issue a Verifiable Credential containing the attributes of an EHIC Card."
+			heading: "eEHIC Issuer",
+			paragraph: "This is a portal where citizens can receive their digital EHIC Card in their wallet. To proceed you must first have an EUDI wallet from a provider of your choice. This Verifiable eEHIC will contain the Social Security Number as your personal identifier for social security.",
+			demo_text: "This is an Issuer implementation for demonstration purposes. The purpose of this application is to issue a Verifiable Credential containing the attributes of an EHIC Card."
 		},
 		login: {
 			title: "Login",
