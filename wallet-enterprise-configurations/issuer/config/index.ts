f9e6c5c0dca2d67cecd178a3ea645fa6d25804d4
kkmanos <koukoularisman@gmail.com>
2025-03-05 11:06:40 +0200

    added new issuer certififacte. added the CA cert on the configuration of each enterprise issuer

--- wallet-enterprise-configurations/issuer/config/index.ts
+++ wallet-enterprise-configurations/issuer/config/index.ts
@@ -18,7 +18,7 @@ export const config = {
 	],
 	issuanceFlow: {
 		skipConsent: false,
-		defaultCredentialConfigurationIds: [ "urn:eu.europa.ec.eudi:pid:1" ],
+		defaultCredentialConfigurationIds: ["urn:eu.europa.ec.eudi:pid:1"],
 		batchCredentialIssuance: {
 			batchSize: 1,
 		}
@@ -28,6 +28,20 @@ export const config = {
 	},
 	appType: 'ISSUER', //ISSUER,VERIFIER
 	wwwalletURL: "http://localhost:3000/cb",
-	trustedRootCertificates: [],
+	trustedRootCertificates: [
+		`-----BEGIN CERTIFICATE-----
+MIICCTCCAa+gAwIBAgIUOgCPRPz+xuyaJVSj4+pw5DL2pcswCgYIKoZIzj0EAwIw
+UjELMAkGA1UEBhMCR1IxDzANBgNVBAgMBkdyZWVjZTEPMA0GA1UEBwwGQXRoZW5z
+MQ4wDAYDVQQKDAVHVW5ldDERMA8GA1UEAwwId3dXYWxsZXQwHhcNMjUwMjI2MTUx
+MjIwWhcNMzUwMjI0MTUxMjIwWjBSMQswCQYDVQQGEwJHUjEPMA0GA1UECAwGR3Jl
+ZWNlMQ8wDQYDVQQHDAZBdGhlbnMxDjAMBgNVBAoMBUdVbmV0MREwDwYDVQQDDAh3
+d1dhbGxldDBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABNBZtjDVXcHAETA8F1Wj
+ScHMtRtfNgYZxYb+6Q5qrOBBRpT75BvaANNoASnnXSXe8HJ4HCB9XG6UuuIhuK/X
+eUejYzBhMA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgEGMB0GA1UdDgQW
+BBR8ePhvPK+ji6cfKwa36k1rRewFojAfBgNVHSMEGDAWgBR8ePhvPK+ji6cfKwa3
+6k1rRewFojAKBggqhkjOPQQDAgNIADBFAiEAiVcSsLpcK6bkYkq03gzejgBQvvKv
+nLP+NsILFBXC+I8CIHSpT1vB/tMaJKbIizZwxyOru6N/iUkpHGzVnxU5Wgu4
+-----END CERTIFICATE-----`
+	],
 	clockTolerance: 60,
 }
