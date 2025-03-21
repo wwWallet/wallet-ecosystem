f7e9228b06be666ec9f3b690af40b23b9bec57d7
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-03-21 13:31:08 +0200

    create new master credential type

--- wallet-enterprise-configurations/issuer/src/configuration/main.ts
+++ wallet-enterprise-configurations/issuer/src/configuration/main.ts
@@ -5,6 +5,7 @@ import { EHICSupportedCredentialSdJwtVCDM } from "./SupportedCredentialsConfigur
 import { PIDSupportedCredentialMsoMdoc } from "./SupportedCredentialsConfiguration/PIDSupportedCredentialMsoMdoc";
 import { PIDSupportedCredentialSdJwtVCDM } from "./SupportedCredentialsConfiguration/PIDSupportedCredentialSdJwtVCDM";
 import { PorSupportedCredentialSdJwt } from "./SupportedCredentialsConfiguration/PorSupportedCredentialSdJwt";
+import { MasterBlueprintSdJwtVCDM} from "./SupportedCredentialsConfiguration/MasterBlueprintSdJwtVCDM";
 
 export async function configurationExecution() {
 	credentialConfigurationRegistryService.register(new PIDSupportedCredentialSdJwtVCDM());
@@ -12,6 +13,7 @@ export async function configurationExecution() {
 	credentialConfigurationRegistryService.register(new EdiplomasBlueprintSdJwtVCDM());
 	credentialConfigurationRegistryService.register(new EHICSupportedCredentialSdJwtVCDM());
 	credentialConfigurationRegistryService.register(new PorSupportedCredentialSdJwt());
+	credentialConfigurationRegistryService.register(new MasterBlueprintSdJwtVCDM());
 	credentialConfigurationRegistryServiceEmitter.emit('initialized');
 }
 
