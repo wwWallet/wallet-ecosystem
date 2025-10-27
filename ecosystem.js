f0089a2fd17872efe6849aae82d7e3f121a955b9
gkatrakazas <grigoriskatrakazas@gmail.com>
2025-10-27 09:06:44 +0200

    remove notification arg

--- ecosystem.js
+++ ecosystem.js
@@ -230,13 +230,6 @@ function buildImages() {
 		configContent = configContent.replace(/WEBAUTHN_RP_ID/g, "localhost");
 		configContent = configContent.replace(/WEBAUTHN_ORIGIN/g, walletClientOrigin);
 
-		// Replacing the whole string so that the value of enabled is of type boolean and not string
-		if (args.includes('--no-notifications')) {
-			configContent = configContent.replace(/enabled: "NOTIFICATIONS_ENABLED"/g, 'enabled: false');
-		} else {
-			configContent = configContent.replace(/enabled: "NOTIFICATIONS_ENABLED"/g, 'enabled: true');
-		}
-
 		fs.writeFileSync(configPath, configContent);
 	}
 }
