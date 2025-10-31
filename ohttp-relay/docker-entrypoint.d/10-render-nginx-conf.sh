f3c44138befc49f6981ce142b7e58e0a7d30269d
Yorgos <dr.george995@gmail.com>
2025-10-31 17:57:29 +0200

    ohttp: seperate config for gateway in docker network

--- ohttp-relay/docker-entrypoint.d/10-render-nginx-conf.sh
+++ ohttp-relay/docker-entrypoint.d/10-render-nginx-conf.sh
@@ -3,11 +3,12 @@ set -eu
 
 # Default if not provided by the environment
 : "${CORS_ALLOWED_ORIGINS:=*}"
+: "${GATEWAY_URL:=http://localhost:4567}"
 
 # Only substitute the vars we intend to change, leave $host etc. intact
-envsubst '${CORS_ALLOWED_ORIGINS}' \
-	< /etc/nginx/conf.d/default.conf.tpl \
-	> /etc/nginx/conf.d/default.conf
+envsubst '${CORS_ALLOWED_ORIGINS} ${GATEWAY_URL}' \
+  < /etc/nginx/conf.d/default.conf.tpl \
+  > /etc/nginx/conf.d/default.conf
 
 rm -f /etc/nginx/conf.d/default.conf.tpl
 
